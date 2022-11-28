5e0845ce672b9383adc7a05a2762ce667224b506
Dustin Turska <86588710+dturska12@users.noreply.github.com>
2022-11-28 17:06:56 -0600

    Add files via upload

---
+++ pages/index.tsx
@@ -0,0 +1,81 @@
+import {
+  ConnectWallet,
+  useAddress,
+  useContract,
+  useContractRead,
+  Web3Button,
+} from "@thirdweb-dev/react";
+import { utils } from "ethers";
+import type { NextPage } from "next";
+import { useState } from "react";
+import styles from "../styles/Home.module.css";
+
+const Home: NextPage = () => {
+  const contractAddress = "0x90e75808Be7Edc6d49feBe26E051d6938C98ab09";
+  const address = useAddress();
+  const [quantity, setQuantity] = useState("2000");
+  const { contract } = useContract(contractAddress);
+  const { data: price, isLoading } = useContractRead(
+    contract,
+    "priceForAddress",
+    address,
+    quantity
+  );
+
+  return (
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Aura LTE {" "}
+          <a href="https://thirdweb.com/nach.eth/DynamicFreeMint/">
+            $ZOE claim
+          </a>
+          !
+        </h1>
+
+        <p className={styles.description}>
+          Simply connect your wallet and press claim
+        </p>
+
+        <div className={styles.connect}>
+          <ConnectWallet />
+        </div>
+
+        <div>
+          <label>
+            Quantity:
+            <input
+              type="number"
+              value={quantity}
+              min="2000"
+              max="2000"
+              onChange={(e) => setQuantity(e.target.value)}
+            />
+          </label>
+        </div>
+
+        <div style={{ marginTop: "10px" }}>
+          <Web3Button
+            contractAddress={contractAddress}
+            action={(contract) => {
+              contract.call("claim", address, quantity, {
+                value: price,
+              });
+            }}
+          >
+            Claim{" "}
+            {price
+              ? `(${
+                  price?.toString() === "0"
+                    ? "Free"
+                    : `${utils.formatEther(price)} ETH`
+                })`
+              : ""}
+          </Web3Button>
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default Home;
